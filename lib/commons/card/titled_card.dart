7ba9ac0c24521c6789f9579ee9ce2c70eaa8840e
Eric Taix <eric.taix@teads.tv>
2020-05-17 01:14:25 +0200

    Same card in dashboard as club's details

--- lib/commons/card/titled_card.dart
+++ lib/commons/card/titled_card.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tinycolor/tinycolor.dart';
 
-class TitledCard extends StatefulWidget {
+class TitledCard extends StatelessWidget {
   final Widget icon;
   final String title;
   final Widget body;
   final List<CardAction> actions;
+  final VoidCallback onTap;
 
-  TitledCard({this.icon, @required this.title, this.body, this.actions}) : assert(title != null);
-
-  @override
-  _TitledCardState createState() => _TitledCardState();
-}
-
-class _TitledCardState extends State<TitledCard> {
+  TitledCard({this.icon, @required this.title, this.body, this.actions, this.onTap}) : assert(title != null);
 
   @override
   Widget build(BuildContext context) {
@@ -35,23 +30,26 @@ class _TitledCardState extends State<TitledCard> {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    if (widget.icon != null)
+                    if (icon != null)
                       Padding(
                         padding: const EdgeInsets.only(right: 18.0),
-                        child: widget.icon,
+                        child: icon,
                       ),
-                    Text(widget.title, style: Theme.of(context).textTheme.caption),
+                    Text(title, style: Theme.of(context).textTheme.caption),
                   ],
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 18.0, right: 8.0, bottom: 18.0, left: 8.0),
-              child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                widget.body ?? SizedBox(height: 100),
-              ]),
+            InkWell(
+              onTap: onTap,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 18.0, right: 8.0, bottom: 18.0, left: 8.0),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                  body ?? SizedBox(height: 100),
+                ]),
+              ),
             ),
-            if (widget.actions != null && widget.actions.isNotEmpty)
+            if (actions != null && actions.isNotEmpty)
               Container(
                 height: 40,
                 width: double.infinity,
@@ -62,7 +60,7 @@ class _TitledCardState extends State<TitledCard> {
                   padding: const EdgeInsets.symmetric(horizontal: 12.0),
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.end,
-                      children: widget.actions
+                      children: actions
                           .map((action) => SizedBox(
                                 width: 50,
                                 child: FlatButton(
