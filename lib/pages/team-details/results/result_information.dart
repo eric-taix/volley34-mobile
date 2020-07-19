455a15f3047da168eca033cec3cba5f5b8b590f8
Thomas Foulon <dev.thomas.foulon@gmail.com>
2020-07-19 14:14:27 +0200

    Change UI to not use InformationCard

--- lib/pages/team-details/results/result_information.dart
+++ lib/pages/team-details/results/result_information.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:v34/models/match_result.dart';
-import 'package:v34/pages/team-details/results/result_information_card.dart';
 
 import 'information_divider.dart';
 
@@ -10,6 +8,8 @@ class ResultInformation extends StatelessWidget {
   final String teamCode;
   final MatchResult result;
 
+  static final double bigScoreSize = 50.0;
+  static final double scoreSize = 30.0;
   static final List<IconData> icons = [Icons.looks_one, Icons.looks_two, Icons.looks_3, Icons.looks_4, Icons.looks_5];
 
   const ResultInformation({Key key, @required this.teamCode, @required this.result}) : super(key: key);
@@ -30,55 +30,113 @@ class ResultInformation extends StatelessWidget {
   }
 
   Widget _buildListItem(BuildContext context, int index, int nbSets) {
-    if (index == 0) return _buildMatchResultInformationCard(context);
-    else if (index == 1) return InformationDivider(title: "Détails du match");
-    else if (index <= nbSets + 1) return _buildSetResultInformationCard(context, index - 1);
-    else if (index == nbSets + 2) return _buildPointSummaryInformationCard(context);
-    else if (index == nbSets + 3) return Container(height: 30,);
-    else return null;
+    if (index == 0) return _buildMatchResult(context);
+    else if (index == 1) return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: InformationDivider(title: "Détails du match"),
+    );
+    else if (index % 2 == 0) {
+      index = index ~/ 2;
+      if (index <= nbSets) return _buildSetResult(context, index);
+      else if (index == nbSets + 1) return _buildPointSummaryResult(context);
+      else return null;
+    } else {
+      return Divider();
+    }
   }
 
-  Widget _buildMatchResultInformationCard(BuildContext context) {
-    String title = "Résultat du match";
-    int hostPoints = result.totalSetsHost;
-    int visitorPoints = result.totalSetsVisitor;
-    return _buildInformationCard(context, title, FontWeight.bold, hostPoints, visitorPoints, true);
+  Widget _buildScore(PointSituation pointSituation, double fontSize) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          pointSituation.hostPoints.toString(),
+          style: TextStyle(color: pointSituation.hostColor, fontWeight: FontWeight.bold, fontSize: fontSize),
+        ),
+        Text(
+            " - ",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: fontSize)
+        ),
+        Text(
+          pointSituation.visitorPoints.toString(),
+          style: TextStyle(color: pointSituation.visitorColor, fontWeight: FontWeight.bold, fontSize: fontSize),
+        ),
+      ],
+    );
   }
 
-  Widget _buildSetResultInformationCard(BuildContext context, int setIndex) {
+  Widget _buildMatchResult(BuildContext context) {
+    PointSituation matchSituation = PointSituation(context, result.totalSetsHost, result.totalSetsVisitor);
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Column(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Text("${result.hostName} ${matchSituation.label}", style: Theme.of(context).textTheme.subtitle1,),
+            )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
+            child: _buildScore(matchSituation, bigScoreSize)
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Text("contre ${result.visitorName}", style: Theme.of(context).textTheme.subtitle1,),
+            )
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSetResult(BuildContext context, int setIndex) {
     if (setIndex > 5) return null;
     if (result.sets[setIndex - 1].hostPoint == null) return null;
-    String title = "Set n°";
-    int hostPoints = result.sets[setIndex - 1].hostPoint;
-    int visitorPoints = result.sets[setIndex - 1].visitorpoint;
-    Icon icon = _buildIndexIcon(context, setIndex);
-    return _buildInformationCard(context, title, FontWeight.normal, hostPoints, visitorPoints, false, icon: icon);
+    PointSituation setSituation = PointSituation(context, result.sets[setIndex - 1].hostPoint, result.sets[setIndex - 1].visitorpoint);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text("Set n°" , style: Theme.of(context).textTheme.headline4,),
+            _buildIndexIcon(context, setIndex)
+          ],
+        ),
+        _buildScore(setSituation, scoreSize)
+      ],
+    );
   }
 
-  Widget _buildPointSummaryInformationCard(BuildContext context) {
-    String title = "Total des points";
+  Widget _buildPointSummaryResult(BuildContext context) {
     int hostPoints = result.totalPointsHost;
     int visitorPoints = result.totalPointsVisitor;
-    return _buildInformationCard(context, title, FontWeight.normal, hostPoints, visitorPoints, false, neutral: true);
-  }
-
-  Widget _buildInformationCard(BuildContext context, String title, FontWeight fontWeight, int hostPoints, int visitorPoints, bool elevation, {Icon icon, bool neutral}) {
-    TextStyle titleStyle = TextStyle(
-        fontSize: 20,
-        fontWeight: fontWeight,
-        color: Theme.of(context).textTheme.bodyText2.color
-    );
-    int diff = hostPoints - visitorPoints;
-    if (teamCode == result.visitorTeamCode) diff = -diff;
-    Color scoreColor;
-    if (neutral != null && neutral) scoreColor = Theme.of(context).textTheme.bodyText2.color;
-    else if (diff == 0) scoreColor = Colors.orange;
-    else if (diff > 0) scoreColor = Colors.green;
-    else scoreColor = Colors.red;
-    return ResultInformationCard(
-      title: title, titleStyle: titleStyle, elevation: elevation,
-      hostName: result.hostName, visitorName: result.visitorName, titleIcon: icon,
-      hostPoints: hostPoints, visitorPoints: visitorPoints, scoreColor: scoreColor
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text("Total des points", style: Theme.of(context).textTheme.headline4,),
+        Row(
+          children: <Widget>[
+            Text(
+              hostPoints.toString(),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: scoreSize),
+            ),
+            Text(
+                " - ",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: scoreSize)
+            ),
+            Text(
+              visitorPoints.toString(),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: scoreSize),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
@@ -86,5 +144,28 @@ class ResultInformation extends StatelessWidget {
     Color color = Theme.of(context).accentColor;
     if (index <= 5) return Icon(icons[index - 1], color: color);
     else return null;
+  }
+}
+
+class PointSituation {
+  String label;
+  int hostPoints, visitorPoints;
+  Color hostColor, visitorColor;
+
+  PointSituation(BuildContext context, this.hostPoints, this.visitorPoints) {
+    int diff = hostPoints - visitorPoints;
+    if (diff > 0){
+      this.label = "gagne";
+      this.hostColor = Colors.green;
+      this.visitorColor = Colors.red;
+    } else if (diff < 0){
+      this.label = "perd";
+      this.hostColor = Colors.red;
+      this.visitorColor = Colors.green;
+    } else {
+      this.label = "fait une égalité";
+      this.hostColor = Theme.of(context).textTheme.bodyText2.color;
+      this.visitorColor = Theme.of(context).textTheme.bodyText2.color;
+    }
   }
 }
