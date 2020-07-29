4f862b4c3800c67d8571d435ba716af3bfa6f0bb
Thomas Foulon <dev.thomas.foulon@gmail.com>
2020-07-29 10:44:35 +0200

    Uses a pie chart to represent match results proportion

--- lib/pages/team-details/ranking/statistics_widget.dart
+++ lib/pages/team-details/ranking/statistics_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_circular_chart/flutter_circular_chart.dart';
+import 'package:tinycolor/tinycolor.dart';
 
 class StatisticsWidget extends StatelessWidget {
   final String title;
@@ -13,67 +14,64 @@ class StatisticsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = this.width ?? 140;
     double radius = width / 5;
-    return Card(
-      elevation: 8.0,
-      child: Container(
-        height: 180,
-        width: width,
-        decoration: BoxDecoration(color: Theme.of(context).cardTheme.color, borderRadius: BorderRadius.all(Radius.circular(18.0))),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-                child: Center(
-                    child: Text(
-                        title,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Theme.of(context).textTheme.bodyText2.color,
-                            fontSize: 20,
-                            fontFamily: "Raleway",
-                            fontWeight: FontWeight.w400
-                        )
-                    )
+    return Container(
+      height: width + 30,
+      width: width,
+      decoration: BoxDecoration(color: Theme.of(context).cardTheme.color, borderRadius: BorderRadius.all(Radius.circular(9.0))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: TinyColor(Theme.of(context).textTheme.bodyText1.color).darken(10).color,
+                    fontSize: 18,
+                    fontFamily: "Raleway",
+                    fontWeight: FontWeight.w300
                 )
-            ),
-            Expanded(
-              flex: 2,
-              child: Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  RichText(
-                    textScaleFactor: 1.0,
-                    textAlign: TextAlign.center,
-                    text: new TextSpan(
-                      text: wonPoints.toString(),
-                      style: new TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).textTheme.bodyText2.color,
-                      ),
-                      children: <TextSpan>[
-                        new TextSpan(text: ' / ${wonPoints + lostPoints}', style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal)),
-                      ],
-                    ),
-                  ),
-                  AnimatedCircularChart(
-                    duration: Duration(milliseconds: 1000),
-                    edgeStyle: SegmentEdgeStyle.round,
-                    size: Size(50.0 + radius*2, 50.0 + radius*2),
-                    initialChartData: [
-                      CircularStackEntry([
-                        CircularSegmentEntry(lostPoints.toDouble(), Colors.redAccent, rankKey: "lost"),
-                        CircularSegmentEntry(wonPoints.toDouble(), Colors.greenAccent, rankKey: "win"),
-                      ])
-                    ],
-                    chartType: CircularChartType.Radial,
-                    holeRadius: radius,
-                  ),
-                ],
-              ),
+              )
             )
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                RichText(
+                  textScaleFactor: 1.0,
+                  textAlign: TextAlign.center,
+                  text: new TextSpan(
+                    text: wonPoints.toString(),
+                    style: new TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).textTheme.bodyText2.color,
+                    ),
+                    children: <TextSpan>[
+                      new TextSpan(text: ' / ${wonPoints + lostPoints}', style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal)),
+                    ],
+                  ),
+                ),
+                AnimatedCircularChart(
+                  duration: Duration(milliseconds: 1000),
+                  edgeStyle: SegmentEdgeStyle.round,
+                  size: Size(50.0 + radius*2, 50.0 + radius*2),
+                  initialChartData: [
+                    CircularStackEntry([
+                      CircularSegmentEntry(lostPoints.toDouble(), Colors.redAccent, rankKey: "lost"),
+                      CircularSegmentEntry(wonPoints.toDouble(), Colors.greenAccent, rankKey: "win"),
+                    ])
+                  ],
+                  chartType: CircularChartType.Radial,
+                  holeRadius: radius,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/pages/team-details/ranking/team_ranking.dart
+++ lib/pages/team-details/ranking/team_ranking.dart
@@ -1,5 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:tinycolor/tinycolor.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 import 'package:v34/commons/podium_widget.dart';
 import 'package:v34/models/classication.dart';
 import 'package:v34/models/team.dart';
@@ -44,59 +45,66 @@ class TeamRanking extends StatelessWidget {
   Widget _buildStats(BuildContext context, ClassificationTeamSynthesis teamStats) {
     return Wrap(
       alignment: WrapAlignment.center,
+      runSpacing: 20,
+      spacing: 20,
       children: <Widget>[
         StatisticsWidget(title: "Matchs gagnés", wonPoints: teamStats.wonMatches, lostPoints: teamStats.lostMatches),
         StatisticsWidget(title: "Set pris", wonPoints: teamStats.wonSets, lostPoints: teamStats.lostSets),
-        StatisticsWidget(title: "Points pris", wonPoints: teamStats.wonPoints, lostPoints: teamStats.lostPoints, width: 200),
+        StatisticsWidget(title: "Points pris", wonPoints: teamStats.wonPoints, lostPoints: teamStats.lostPoints, width: 180),
       ],
     );
   }
 
   Widget _buildResultsSummary(BuildContext context, ClassificationTeamSynthesis teamStats) {
-    return Wrap(
-      alignment: WrapAlignment.spaceAround,
-      runSpacing: 20,
-      children: <Widget>[
-        _buildMiniResult(context, "3 - 0", teamStats.nbSets30),
-        _buildMiniResult(context, "3 - 1", teamStats.nbSets31),
-        _buildMiniResult(context, "3 - 2", teamStats.nbSets32),
-        _buildMiniResult(context, "0 - 3", teamStats.nbSets03),
-        _buildMiniResult(context, "1 - 3", teamStats.nbSets13),
-        _buildMiniResult(context, "2 - 3", teamStats.nbSets23),
-        _buildMiniResult(context, "2 - 2", teamStats.nbSets22),
-      ],
-    );
-  }
-  
-  Widget _buildMiniResult(BuildContext context, String title, int points) {
-    Color color = Theme.of(context).cardTheme.color;
-    Color darkenedColor = TinyColor(color).darken(5).color;
-    return Container(
-      height: 50,
-      width: 100,
-      decoration: BoxDecoration(color: darkenedColor, borderRadius: BorderRadius.all(Radius.circular(20.0))),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            flex: 6,
-            child: Container(
-              child: Text(title, textAlign: TextAlign.center, style: Theme.of(context).textTheme.bodyText2.apply(fontWeightDelta: 2, fontSizeFactor: 1.3))
-            ),
-          ),
-          Expanded(
-            flex: 4,
-            child: SizedBox(
-              height: 50,
-              child: Container(
-                decoration: BoxDecoration(color: color, borderRadius: BorderRadius.only(topRight: Radius.circular(20.0), bottomRight: Radius.circular(20.0))),
-                child: Center(child: Text(points.toString(), textAlign: TextAlign.center, style: Theme.of(context).textTheme.bodyText2.apply(fontWeightDelta: 2, fontSizeFactor: 1.7)))
-              )
-            ),
-          )
-        ],
-      ),
-    );
+    TextStyle style = TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15);
+    return PieChart(PieChartData(
+      borderData: FlBorderData(border: Border.all(width: 0.0)),
+      centerSpaceRadius: 80,
+      sections: [
+        if (teamStats.nbSets30 > 0) PieChartSectionData(
+          color: Colors.greenAccent,
+          title: "3-0",
+          value: teamStats.nbSets30.toDouble(),
+          titleStyle: style
+        ),
+        if (teamStats.nbSets31 > 0) PieChartSectionData(
+          color: Colors.cyanAccent,
+          title: "3-1",
+          value: teamStats.nbSets31.toDouble(),
+          titleStyle: style
+        ),
+        if (teamStats.nbSets32 > 0) PieChartSectionData(
+          color: Colors.blueAccent,
+          title: "3-2",
+          value: teamStats.nbSets32.toDouble(),
+          titleStyle: style
+        ),
+        if (teamStats.nbSets22 > 0) PieChartSectionData(
+          color: Colors.black38,
+          title: "2-2",
+          value: teamStats.nbSets22.toDouble(),
+          titleStyle: style
+        ),
+        if (teamStats.nbSets03 > 0) PieChartSectionData(
+          color: Colors.redAccent,
+          title: "0-3",
+          value: teamStats.nbSets03.toDouble(),
+          titleStyle: style
+        ),
+        if (teamStats.nbSets13 > 0) PieChartSectionData(
+          color: Colors.orangeAccent,
+          title: "1-3",
+          value: teamStats.nbSets13.toDouble(),
+          titleStyle: style
+        ),
+        if (teamStats.nbSets23 > 0) PieChartSectionData(
+          color: Colors.yellowAccent,
+          title: "2-3",
+          value: teamStats.nbSets23.toDouble(),
+          titleStyle: style
+        ),
+      ]
+    ));
   }
 
 }
