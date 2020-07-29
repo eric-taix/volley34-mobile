83c79e98a35a0a8528480b70ede35ce22073e535
Thomas Foulon <dev.thomas.foulon@gmail.com>
2020-07-29 14:26:06 +0200

    Adapting UI by using different charts for team ranking

--- lib/pages/team-details/ranking/statistics_widget.dart
+++ lib/pages/team-details/ranking/statistics_widget.dart
@@ -1,76 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_circular_chart/flutter_circular_chart.dart';
-import 'package:tinycolor/tinycolor.dart';
+import 'package:v34/commons/graphs/arc.dart';
 
 class StatisticsWidget extends StatelessWidget {
   final String title;
-  final double width;
   final int wonPoints;
   final int lostPoints;
 
-  const StatisticsWidget({Key key, @required this.wonPoints, @required this.lostPoints, @required this.title, this.width}) : super(key: key);
+  static final double miniGraphSize = 60;
+
+  const StatisticsWidget({Key key, @required this.wonPoints, @required this.lostPoints, @required this.title}) : super(key: key);
+
+  Widget _buildRatio(BuildContext context) {
+    return RichText(
+      textScaleFactor: 1.2,
+      textAlign: TextAlign.center,
+      text: new TextSpan(
+        text: wonPoints.toString(),
+        style: new TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+          color: Theme.of(context).textTheme.bodyText2.color,
+        ),
+        children: <TextSpan>[
+          new TextSpan(text: ' / ${wonPoints + lostPoints}', style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGraph(BuildContext context) {
+    return SizedBox(
+      height: miniGraphSize,
+      width: miniGraphSize,
+      child: ArcGraph(
+        minValue: 0,
+        maxValue: (wonPoints + lostPoints).toDouble(),
+        value: wonPoints.toDouble(),
+        lineWidth: 6,
+        leftTitle: LeftTitle(
+          show: true,
+          text: "",
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+        valueBuilder: (value, minValue, maxValue) {
+          var percentage = maxValue != 0 ? "${(((value - minValue) / maxValue) * 100).toStringAsFixed(1)}%" : "- -";
+          return Text(percentage);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    double width = this.width ?? 140;
-    double radius = width / 5;
     return Container(
-      height: width + 30,
-      width: width,
-      decoration: BoxDecoration(color: Theme.of(context).cardTheme.color, borderRadius: BorderRadius.all(Radius.circular(9.0))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      padding: EdgeInsets.symmetric(vertical: 5.0),
+      child: Row(
         children: <Widget>[
-          Expanded(
-            child: Center(
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: TinyColor(Theme.of(context).textTheme.bodyText1.color).darken(10).color,
-                    fontSize: 18,
-                    fontFamily: "Raleway",
-                    fontWeight: FontWeight.w300
-                )
-              )
-            )
-          ),
-          Expanded(
-            flex: 2,
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                RichText(
-                  textScaleFactor: 1.0,
-                  textAlign: TextAlign.center,
-                  text: new TextSpan(
-                    text: wonPoints.toString(),
-                    style: new TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).textTheme.bodyText2.color,
-                    ),
-                    children: <TextSpan>[
-                      new TextSpan(text: ' / ${wonPoints + lostPoints}', style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal)),
-                    ],
-                  ),
-                ),
-                AnimatedCircularChart(
-                  duration: Duration(milliseconds: 1000),
-                  edgeStyle: SegmentEdgeStyle.round,
-                  size: Size(50.0 + radius*2, 50.0 + radius*2),
-                  initialChartData: [
-                    CircularStackEntry([
-                      CircularSegmentEntry(lostPoints.toDouble(), Colors.redAccent, rankKey: "lost"),
-                      CircularSegmentEntry(wonPoints.toDouble(), Colors.greenAccent, rankKey: "win"),
-                    ])
-                  ],
-                  chartType: CircularChartType.Radial,
-                  holeRadius: radius,
-                ),
-              ],
-            ),
-          )
+          Expanded(child: Text(title, textAlign: TextAlign.center, style: Theme.of(context).textTheme.subtitle2)),
+          Expanded(child: _buildRatio(context)),
+          Expanded(child: _buildGraph(context)),
         ],
       ),
     );
--- lib/pages/team-details/ranking/team_ranking.dart
+++ lib/pages/team-details/ranking/team_ranking.dart
@@ -1,11 +1,11 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 import 'package:v34/commons/podium_widget.dart';
 import 'package:v34/models/classication.dart';
 import 'package:v34/models/team.dart';
 import 'package:v34/pages/team-details/information_divider.dart';
 import 'package:v34/pages/team-details/ranking/statistics_widget.dart';
+import 'package:v34/utils/extensions.dart';
 
 class TeamRanking extends StatelessWidget {
   final Team team;
@@ -43,68 +43,83 @@ class TeamRanking extends StatelessWidget {
   }
 
   Widget _buildStats(BuildContext context, ClassificationTeamSynthesis teamStats) {
-    return Wrap(
-      alignment: WrapAlignment.center,
-      runSpacing: 20,
-      spacing: 20,
+    return Column(
       children: <Widget>[
         StatisticsWidget(title: "Matchs gagnés", wonPoints: teamStats.wonMatches, lostPoints: teamStats.lostMatches),
         StatisticsWidget(title: "Set pris", wonPoints: teamStats.wonSets, lostPoints: teamStats.lostSets),
-        StatisticsWidget(title: "Points pris", wonPoints: teamStats.wonPoints, lostPoints: teamStats.lostPoints, width: 180),
+        StatisticsWidget(title: "Points pris", wonPoints: teamStats.wonPoints, lostPoints: teamStats.lostPoints),
+
       ],
     );
   }
 
   Widget _buildResultsSummary(BuildContext context, ClassificationTeamSynthesis teamStats) {
-    TextStyle style = TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15);
-    return PieChart(PieChartData(
-      borderData: FlBorderData(border: Border.all(width: 0.0)),
-      centerSpaceRadius: 80,
-      sections: [
-        if (teamStats.nbSets30 > 0) PieChartSectionData(
-          color: Colors.greenAccent,
-          title: "3-0",
-          value: teamStats.nbSets30.toDouble(),
-          titleStyle: style
+    return SizedBox(
+      height: 150,
+      child: FractionallySizedBox(
+        widthFactor: 0.5,
+        child: BarChart(BarChartData(
+          borderData: FlBorderData(show: false),
+          titlesData: FlTitlesData(
+            show: true,
+            bottomTitles: SideTitles(
+              showTitles: true,
+              textStyle: Theme.of(context).textTheme.bodyText2.apply(fontWeightDelta: 2),
+              margin: 16,
+              getTitles: (double value) {
+                switch (value.toInt()) {
+                  case 0: return '3-0\n${teamStats.nbSets30 > 0 ? teamStats.nbSets30.toString() : "-"}';
+                  case 1: return '3-1\n${teamStats.nbSets31 > 0 ? teamStats.nbSets31.toString() : "-"}';
+                  case 2: return '3-2\n${teamStats.nbSets32 > 0 ? teamStats.nbSets32.toString() : "-"}';
+                  case 3: return '2-3\n${teamStats.nbSets23 > 0 ? teamStats.nbSets23.toString() : "-"}';
+                  case 4: return '1-3\n${teamStats.nbSets13 > 0 ? teamStats.nbSets13.toString() : "-"}';
+                  case 5: return '0-3\n${teamStats.nbSets03 > 0 ? teamStats.nbSets03.toString() : "-"}';
+                  default: return '';
+                }
+              },
+            ),
+            leftTitles: SideTitles(showTitles: false),
+          ),
+          barGroups: showingGroups(context, teamStats),
+        )),
+      ),
+    );
+  }
+
+  List<BarChartGroupData> showingGroups(BuildContext context, ClassificationTeamSynthesis teamStats) {
+    return List.generate(6, (i) {
+     switch (i) {
+       case 0:
+         return makeGroupData(context, 0, teamStats.nbSets30.toDouble(), Colors.green, teamStats);
+       case 1:
+         return makeGroupData(context, 1, teamStats.nbSets31.toDouble(), Colors.greenAccent, teamStats);
+       case 2:
+         return makeGroupData(context, 2, teamStats.nbSets32.toDouble(), Colors.yellowAccent, teamStats);
+       case 3:
+         return makeGroupData(context, 3, teamStats.nbSets23.toDouble(), Colors.orangeAccent, teamStats);
+       case 4:
+         return makeGroupData(context, 4, teamStats.nbSets13.toDouble(), Colors.deepOrangeAccent, teamStats);
+       case 5:
+         return makeGroupData(context, 5, teamStats.nbSets03.toDouble(), Colors.redAccent, teamStats);
+       default:
+         return null;
+     }
+    });
+  }
+
+  BarChartGroupData makeGroupData(BuildContext context, int x, double y, Color barColor, ClassificationTeamSynthesis teamStats) {
+    return BarChartGroupData(
+      x: x,
+      barRods: [
+        BarChartRodData(y: y, color: barColor, width: 14,
+          backDrawRodData: BackgroundBarChartRodData(
+            show: true,
+            y: (teamStats.wonMatches + teamStats.lostMatches).toDouble(),
+            color: Theme.of(context).cardTheme.color.tiny(10), //barBackgroundColor,
+          ),
         ),
-        if (teamStats.nbSets31 > 0) PieChartSectionData(
-          color: Colors.cyanAccent,
-          title: "3-1",
-          value: teamStats.nbSets31.toDouble(),
-          titleStyle: style
-        ),
-        if (teamStats.nbSets32 > 0) PieChartSectionData(
-          color: Colors.blueAccent,
-          title: "3-2",
-          value: teamStats.nbSets32.toDouble(),
-          titleStyle: style
-        ),
-        if (teamStats.nbSets22 > 0) PieChartSectionData(
-          color: Colors.black38,
-          title: "2-2",
-          value: teamStats.nbSets22.toDouble(),
-          titleStyle: style
-        ),
-        if (teamStats.nbSets03 > 0) PieChartSectionData(
-          color: Colors.redAccent,
-          title: "0-3",
-          value: teamStats.nbSets03.toDouble(),
-          titleStyle: style
-        ),
-        if (teamStats.nbSets13 > 0) PieChartSectionData(
-          color: Colors.orangeAccent,
-          title: "1-3",
-          value: teamStats.nbSets13.toDouble(),
-          titleStyle: style
-        ),
-        if (teamStats.nbSets23 > 0) PieChartSectionData(
-          color: Colors.yellowAccent,
-          title: "2-3",
-          value: teamStats.nbSets23.toDouble(),
-          titleStyle: style
-        ),
-      ]
-    ));
+      ],
+    );
   }
 
 }
