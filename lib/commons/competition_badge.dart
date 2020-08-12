6f0fbbed4679dd769590eaa3e7623ad80b36375f
Eric Taix <eric.taix@teads.tv>
2020-08-12 17:25:36 +0200

    Add a label under the competition badge to explain it

--- lib/commons/competition_badge.dart
+++ lib/commons/competition_badge.dart
@@ -10,51 +10,66 @@ class CompetitionBadge extends StatelessWidget {
 
   _CompetitionBadgePainter _drawCompetitionBadge(BuildContext context) {
     String gameType = competitionCode.substring(competitionCode.length - 1);
+    TextStyle subTitleStyle = Theme.of(context).textTheme.bodyText1;
     switch (int.parse(gameType)) {
       case 1:
         return _CompetitionBadgePainter(
             context: context,
             label: "4",
             leftColor: Colors.blue,
-            rightColor: Colors.blue);
+            rightColor: Colors.blue,
+            subtitleStyle: subTitleStyle,
+            subtitle: "4x4 masculin");
       case 2:
         return _CompetitionBadgePainter(
             context: context,
             label: "6",
             leftColor: Colors.blue,
-            rightColor: Colors.blue);
+            rightColor: Colors.blue,
+            subtitleStyle: subTitleStyle,
+            subtitle: "6x6 masculin");
       case 3:
       case 4:
         return _CompetitionBadgePainter(
             context: context,
             label: "4",
             leftColor: Colors.blue,
-            rightColor: Colors.pinkAccent);
+            rightColor: Colors.pinkAccent,
+            subtitleStyle: subTitleStyle,
+            subtitle: "4x4 mixte");
       case 5:
         return _CompetitionBadgePainter(
             context: context,
             label: "6",
             leftColor: Colors.blue,
-            rightColor: Colors.pinkAccent);
+            rightColor: Colors.pinkAccent,
+            subtitleStyle: subTitleStyle,
+            subtitle: "6x6 mixte");
       case 6:
         return _CompetitionBadgePainter(
             context: context,
             label: "4",
             leftColor: Colors.pinkAccent,
-            rightColor: Colors.pinkAccent);
+            rightColor: Colors.pinkAccent,
+            subtitleStyle: subTitleStyle,
+            subtitle: "4x4 féminin");
       case 7:
         return _CompetitionBadgePainter(
             context: context,
             label: "6",
             leftColor: Colors.pinkAccent,
-            rightColor: Colors.pinkAccent);
+            rightColor: Colors.pinkAccent,
+            subtitleStyle: subTitleStyle,
+            subtitle: "6x6 féminin");
       default:
         // throw new Exception("Type de compétition inconnue.");
         return _CompetitionBadgePainter(
             context: context,
-            label: "4",
+            label: "?",
             leftColor: Colors.blue,
-            rightColor: Colors.blue);
+            rightColor: Colors.blue,
+            subtitleStyle: subTitleStyle,
+            subtitle: "inconnu");
     }
   }
 
@@ -73,11 +88,15 @@ class _CompetitionBadgePainter extends CustomPainter {
   final String label;
   final Color leftColor;
   final Color rightColor;
+  final String subtitle;
+  final TextStyle subtitleStyle;
 
   _CompetitionBadgePainter(
       {@required this.context,
       @required this.label,
       @required this.leftColor,
+      @required this.subtitle,
+      @required this.subtitleStyle,
       Color rightColor})
       : this.rightColor = rightColor ?? leftColor;
 
@@ -111,7 +130,6 @@ class _CompetitionBadgePainter extends CustomPainter {
         Offset(demiHeight, demiHeight), Offset(demiHeight, demiHeight), left);
     canvas.drawLine(
         Offset(demiHeight, demiHeight), Offset(height, demiHeight), leftSquare);
-
     canvas.drawLine(Offset(width - demiHeight, demiHeight),
         Offset(width - demiHeight, demiHeight), right);
     canvas.drawLine(Offset(width - demiHeight, demiHeight),
@@ -123,13 +141,22 @@ class _CompetitionBadgePainter extends CustomPainter {
         text: span,
         textAlign: TextAlign.left,
         textDirection: TextDirection.ltr);
-
     tp.layout();
-
     tp.paint(
         canvas, Offset(demiHeight - tp.width / 2, demiHeight - tp.height / 2));
     tp.paint(canvas,
         Offset(width - demiHeight - tp.width / 2, demiHeight - tp.height / 2));
+
+    TextSpan subtitleSpan = TextSpan(text: subtitle, style: subtitleStyle);
+    TextPainter subtitleTp = TextPainter(
+        text: subtitleSpan,
+        textAlign: TextAlign.left,
+        textDirection: TextDirection.ltr);
+    subtitleTp.layout();
+    subtitleTp.paint(
+        canvas,
+        Offset(size.width / 2 - subtitleTp.width / 2,
+            height + subtitleTp.height / 4));
   }
 
   @override
